dc1aa4ee04783e8b6dd229e01f125c02ebc7ba30
Vi Pham <tvpham11@gmail.com>
2015-06-17 16:11:36 -0400

    set up backbone directories and files

---
+++ app/js/setup.js
@@ -0,0 +1,24 @@
+;(function () {
+
+  'use strict';
+
+   // Globals
+   window.app = {};
+   window.app.Models = {};
+   window.app.Collections = {};
+   window.app.Views = {};
+   window.app.Routers = {};
+
+   // Root URL
+   app.rootURL = appEndpoint;
+
+  // For auth. token
+  /*$.ajaxSetup({
+
+    headers: {
+      "Authorization" : "12345" // whatever value is stored in the cookie
+    }
+
+  });*/
+
+}());
