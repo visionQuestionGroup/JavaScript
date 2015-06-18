9ea50c391fad1bccee8bc599c3d6b8ddfa1c9447
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-17 21:56:09 -0400

    Styled placeholder nav. Added base for main.js

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,20 @@
-console.log('This is cool!');
+;(function (){
 
-$('.container').html(template.example({
-  value: 'Hello, World!!'
-}));
+  'use strict';
+
+  var allUsers = new app.Collections.Users();
+  // var allGuesses = new app.Collections.Guesses();
+  // var allPosts = new app.Collections.Posts();
+
+
+
+  // allPosts.fetch().done( function () {
+
+  //   app.mainRouter = new app.Routers.MainRouter({
+  //     collection: allPosts
+  //   });
+    Backbone.history.start();
+
+  // });
+
+}());
