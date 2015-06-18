5f78c4c3986028c9d061bf79b703decbd50ec044
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-17 20:01:39 -0400

    Added User collection

---
+++ app/js/collections/users.collection.js
@@ -0,0 +1,15 @@
+;(function (){
+
+  'use strict';
+
+  app.Collections.Users = Backbone.Collection.extend({
+
+    model: app.Models.User,
+
+    comparator: 'username',
+
+    url: app.rootURL
+
+  });
+
+}());
