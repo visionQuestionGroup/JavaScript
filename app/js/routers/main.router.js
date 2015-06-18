d307a28cacd07026d086de2f6223d6732f780de8
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-17 20:21:51 -0400

    Started on router file.  Added signup route and error page routes

---
+++ app/js/routers/main.router.js
@@ -0,0 +1,34 @@
+;(function (){
+
+  'use strict';
+
+  app.Routers.MainRouter = Backbone.Router.extend({
+
+    initialize: function (options) {
+
+      this.collection = options.collection;
+
+    },
+
+      routes: {
+      '' : 'homePage',
+      'visionQuestion/:id' : 'viewQuestion',
+      'scoreboard' : 'scoreboard',
+      'newQuest' : 'newQuest',
+      'signup' : 'signUp',
+      // 'edit/:id' : 'editQuest',
+      '*path' : 'errorPage'
+    },
+
+    signUp: function () {
+      new app.Views.signup();
+    },
+
+    errorPage: function () {
+      $('.container').html('404 Not Found');
+    }
+
+    });
+
+
+  }());
