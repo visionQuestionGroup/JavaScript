a1af94253e0704f75ec24c8f3675215fe53d99b4
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-19 14:00:33 -0400

    Transfered the nav bar to a handlebars template that is built in to each view

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,16 @@
 
   'use strict';
 
+  //Check for login
+
+  var isLoggedIn = Cookies.get('access_token');
+
+  if ( isLoggedIn !== undefined ){
+    console.log('Yep, logged in');
+   } else {
+    console.log('Nope, not logged in');
+   }
+
   // var allGuesses = new app.Collections.Guesses();
   // var allPosts = new app.Collections.Posts();
 
@@ -11,7 +21,7 @@
 
   app.mainRouter = new app.Routers.MainRouter({
       // collection: allPosts
-    });
+  });
 
   Backbone.history.start();
 
--- app/js/views/signup.view.js
+++ app/js/views/signup.view.js
@@ -3,6 +3,32 @@
   'use strict';
 
 
+  app.Views.NavView = Backbone.View.extend({
+
+    className: 'nav',
+
+    template: hbs.nav,
+
+    initialize: function (options) {
+
+      var args = options || {};
+
+      this.render();
+
+      $('#nav').html(this.el);
+
+    },
+
+    render: function(){
+
+      this.$el.html(this.template);
+
+    }
+
+  });
+
+
+
   app.Views.SignUpView = Backbone.View.extend({
 
     className: 'signUp',
