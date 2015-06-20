95b9f94038c1020fbbb3b74a6b33bd72a0dbba4a
Meade Slocomb <meade.slocomb@gmail.com>
2015-06-20 00:38:20 -0400

    Fixed nav view bug, added code for sign in post request, tweaked nav bar view

--- app/js/views/main.view.js
+++ app/js/views/main.view.js
@@ -2,29 +2,6 @@
 
   'use strict';
 
-  app.Views.NavView = Backbone.View.extend({
-
-    className: 'nav',
-
-    template: hbs.nav,
-
-  initialize: function (options) {
-
-    var args = options || {};
-
-    this.render();
-
-    $('#nav').html(this.el);
-  },
-
-  render: function(){
-
-    this.$el.html(this.template);
-
-  }
-
-  });
-
   app.Views.Main = Backbone.View.extend({
 
     className: 'main',
